83fdea9d0d75855e80257bd500732b4860b734d4
Elia <elia.ravera.lavoro@gmail.com>
2025-03-12 11:33:39 +0100

    Automated deployment

--- cjs/index.d.ts
+++ cjs/index.d.ts
@@ -1,4 +1,3 @@
 import Socket from "./classes/socket.class";
 import Server from "./classes/server.class";
-import Client from "./classes/client.class";
-export { Server, Socket, Client };
+export { Server, Socket };
